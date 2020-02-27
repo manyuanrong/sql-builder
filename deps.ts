0e97af4fceac83d4a8cf6e8f23ab3ed6d5fcd05a
boyce <boyce51532853@gmail.com>
2020-02-27 19:03:56 +0800

    Update deps.ts

--- deps.ts
+++ deps.ts
@@ -1,3 +1,3 @@
 export { assert, assertEquals } from "https://deno.land/std/testing/asserts.ts";
-export { runTests, test } from "https://deno.land/std/testing/mod.ts";
+export { runTests, test } from "https://deno.land/std@v0.24.0/testing/mod.ts";
 export { replaceParams } from "./util.ts";
