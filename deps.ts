26c06c84556dba65e08f568077937eead12616e6
Chris Couzens <ccouzens@gmail.com>
2020-09-23 20:44:20 +0100

    Fix import warning
    When running `deno test` I got the following warning:
    
    > Warning std versions prefixed with 'v' will be deprecated on October 1st 2020. Please change your import to https://deno.land/std@0.51.0/testing/asserts.ts (at https://deno.land/std@v0.51.0/testing/asserts.ts)

--- deps.ts
+++ deps.ts
@@ -1,5 +1,5 @@
 export {
   assert,
   assertEquals,
-} from "https://deno.land/std@v0.51.0/testing/asserts.ts";
+} from "https://deno.land/std@0.51.0/testing/asserts.ts";
 export { replaceParams } from "./util.ts";
